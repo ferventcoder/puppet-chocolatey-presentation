d456014a83c87d08ea5898b69b1c0290b956dec0
Rob Reynolds <ferventcoder@gmail.com>
2016-10-21 13:14:03 -0500

    (demo) add MSI template

---
+++ demo/resources/templates/MSI/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+[[AutomaticPackageNotesInstaller]]
+$packageName  = '[[PackageName]]'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  file          = $fileLocation
+  fileType      = 'MSI' #only one of these: exe, msi, msu [[InstallerType]]
+
+  #MSI
+  silentArgs    = "/qn /norestart USE_SETTINGS_FROM_AD=0 /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+  #OTHERS
+  #silentArgs   ='[[SilentArgs]]' # /s /S /q /Q /quiet /silent /SILENT /VERYSILENT -s - try any of these to get the silent installer
+  #validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+Install-ChocolateyInstallPackage @packageArgs
