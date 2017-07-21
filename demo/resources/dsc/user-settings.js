53f302b6f01c3e2704038467c659b6ec0bbc7c2e
Rob Reynolds <ferventcoder@gmail.com>
2017-07-21 14:14:25 -0500

    Add user settings for vscode

---
+++ demo/resources/dsc/user-settings.js
@@ -0,0 +1,45 @@
+// Place your settings in this file to overwrite the default settings
+{
+
+  // The default end of line character.
+	"files.eol": "\n",
+
+	// When enabled, will trim trailing whitespace when you save a file.
+	"files.trimTrailingWhitespace": true,
+
+	// Controls auto save of dirty files. Accepted values:  "off", "afterDelay", "onFocusChange". If set to "afterDelay" you can configure the delay in "files.autoSaveDelay".
+	"files.autoSave": "onFocusChange",
+
+  // Whether auto fetching is enabled.
+	"git.autofetch": false,
+
+  // Columns at which to show vertical rulers
+	"editor.rulers": [50, 72, 80, 120],
+
+  // The number of spaces a tab is equal to.
+	"editor.tabSize": 2,
+
+  // Enable usage data and errors to be sent to Microsoft.
+	"telemetry.enableTelemetry": false,
+
+  // Enable crash reports to be sent to Microsoft.
+	// This option requires restart to take effect.
+	"telemetry.enableCrashReporter": false,
+
+  "files.insertFinalNewline": true,
+
+    // Configure file associations to languages (e.g. "*.extension": "html"). These have precedence over the default associations of the languages installed.
+	"files.associations": {
+    "Vagrantfile": "ruby",
+    "*.pp":"puppet"
+  },
+  "workbench.colorTheme": "Solarized Light",
+
+  // Depth control [1-6].
+  "markdown-toc.depthFrom": 2,
+
+  "editor.minimap.enabled": true,
+  "editor.dragAndDrop": true,
+  "editor.renderWhitespace": "all",
+  "terminal.integrated.shell.windows": "C:\\Windows\\Sysnative\\WindowsPowerShell\\v1.0\\powershell.exe"
+}
