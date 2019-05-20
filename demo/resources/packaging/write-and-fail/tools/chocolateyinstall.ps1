21a47bbc598707ef6aa3b18f9eb33d5f8c667c09
Rob Reynolds <ferventcoder@gmail.com>
2019-05-20 10:04:24 -0500

    (packaging) add failing package

---
+++ demo/resources/packaging/write-and-fail/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "This is a host level message"
+Write-Debug "This is a debug level message"
+Write-Verbose "This is a verbose level message"
+Write-Warning "This is a warning level message"
+Write-Error "This is an error level message"
+
+throw "Failing the package"
+
